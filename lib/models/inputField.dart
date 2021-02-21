364ff48d59cc39cd4c843c0cd4dc38226262e171
Oskar Kellermann <oskar@kellermann.team>
2021-02-21 13:54:04 +0100

    bug fix

--- lib/models/inputField.dart
+++ lib/models/inputField.dart
@@ -27,10 +27,10 @@ class InputField extends StatelessWidget {
         decoration: InputDecoration(
           suffix: Container(
             margin: EdgeInsets.only(right: 25),
-            child: Center(child: suffixWidget)
+            child: suffixWidget
           ),
           labelText: hintText,
-          labelStyle: TextStyle(fontSize: 14, color: Color(0xff63f1b3)),
+          labelStyle: TextStyle(fontSize: 14),
           hintText: hintText,
           prefixIcon: icon,
           border: InputBorder.none,

e0da148fb9ebfebc7f46a91cceb806b97d516d25
Oskar Kellermann <oskar@kellermann.team>
2021-03-27 11:41:21 +0100

    new icon and new launchscreen

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,6 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.white60, //(0xff8e84cf),
         shadowColor: Color(0xff131415),
         buttonColor: Color(0xffc083ff),
-        //highlightColor: Color(0xffc083ff),
-        //splashColor: Color(0xffc083ff),
         indicatorColor: Color(0xff23252d),
         cardColor: Color(0xff1f2022), // Color(0xff23252d),
         primaryColor: Color(0xffff6a61),
